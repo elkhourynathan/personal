732d9fec79424031e7bcf8436f5c3e795b606bca
elkhourynathan <elkhourynathan@gmail.com>
2024-05-06 15:36:14 -0400

    Fix scroll in navbar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -17,7 +17,7 @@ export function Navbar() {
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-start justify-between relative px-0 pb-0 fade scroll-pr-6 md:relative"
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
@@ -26,7 +26,7 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2"
+                  className="transition-all hover:text-neutral-200 flex align-middle relative py-1 px-2"
                 >
                   {name}
                 </Link>
