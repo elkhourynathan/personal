42c684fed713b232d58e2b5ed52a8f7f39859637
elkhourynathan <elkhourynathan@gmail.com>
2024-06-03 23:07:32 -0400

    Updated with new project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,64 +11,89 @@ export const metadata: Metadata = {
 export default function ProjectsPage() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
-        Projects
-      </h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">Projects</h1>
       <div className="prose prose-neutral ">
         <hr className="my-6 border-neutral-600 " />
         <ProjectCardHosted
-            title="Machine Learning UFC Predictor"
-            date="2024"
-            description="Developed a project that compiles data from all previous UFC fights and fighters, utilizing this information to train a 
+          title="Machine Learning UFC Predictor"
+          date="2024"
+          description="Developed a project that compiles data from all previous UFC fights and fighters, utilizing this information to train a 
             machine learning model that predicts the outcomes of upcoming fights."
-            techStack={['Python', 'Sci-kit Learn', 'Pandas', 'Numpy', 'ReactJS', 'Flask', 'SQL']}
-            imageUrl="/images/ml_ufc_pred.png"
-            githubUrl="https://github.com/elkhourynathan/fight_predictor"
-            hostedUrl="https://ufc-fight-pred-00326a93d210.herokuapp.com/"
+          techStack={[
+            'Python',
+            'Sci-kit Learn',
+            'Pandas',
+            'Numpy',
+            'ReactJS',
+            'Flask',
+            'SQL',
+          ]}
+          imageUrl="/images/ml_ufc_pred.png"
+          githubUrl="https://github.com/elkhourynathan/fight_predictor"
+          hostedUrl="https://ufc-fight-pred-00326a93d210.herokuapp.com/"
+        />
+      </div>
+      <div className="prose prose-neutral ">
+        <hr className="my-6 border-neutral-600 " />
+        <ProjectCard
+          title="Computer Vision Poker Game Theory Optimizer"
+          date="2024"
+          description="I developed a poker game theory optimizer in Python for poker video analytics. This project involved creating a 
+          sophisticated video analytics pipeline using a custom-trained YOLOv8 model and ByteTrack to accurately distinguish and track 
+          player cards from community cards. Additionally, I implemented a Monte Carlo simulation to calculate the probabilities of winning, 
+          tying, and losing, which enables strategic decisions on whether to fold, call, or raise based on the expected value.."
+          techStack={[
+            'Python',
+            'Ultralytics',
+            'OpenCV',
+            'Supervision',
+          ]}
+          imageUrl="/images/poker_cv_image.png"
+          githubUrl="https://github.com/elkhourynathan/poker_game_theory_optimizer"
         />
       </div>
       <div className="prose prose-neutral">
         <ProjectCard
-            title="Health and Fitness Club Management System"
-            date="2024"
-            description="Designed a web application that facilitates interaction among various user categories including members, trainers, and administrators. 
+          title="Health and Fitness Club Management System"
+          date="2024"
+          description="Designed a web application that facilitates interaction among various user categories including members, trainers, and administrators. 
             This platform replicates essential functions needed to manage a fitness club, like personal logging, scheduling member and trainer sessions, 
             organizing classes, and maintaining equipment."
-            techStack={['Python', 'PostgreSQL', 'Flask']}
-            imageUrl="/images/health_fitness_management.png"
-            githubUrl="https://github.com/elkhourynathan/comp3005_project_v2"
+          techStack={['Python', 'PostgreSQL', 'Flask']}
+          imageUrl="/images/health_fitness_management.png"
+          githubUrl="https://github.com/elkhourynathan/comp3005_project_v2"
         />
       </div>
       <div className="prose prose-neutral">
         <ProjectCard
-            title="Workout Tracker"
-            date="2024"
-            description="Created a web application that enables users to register or log in and monitor intricate workout routines, 
+          title="Workout Tracker"
+          date="2024"
+          description="Created a web application that enables users to register or log in and monitor intricate workout routines, 
             encompassing varied exercise plans, specific sets, repetitions, and weights. 
             This application helps users precisely track their fitness journey and observe their progress over time."
-            techStack={['MongoDB', 'Expressjs', 'ReactJS', 'NodeJS']}
-            imageUrl="/images/wrkpr_login.png"
-            githubUrl="https://github.com/elkhourynathan/workouttracker_wrkpr"
+          techStack={['MongoDB', 'Expressjs', 'ReactJS', 'NodeJS']}
+          imageUrl="/images/wrkpr_login.png"
+          githubUrl="https://github.com/elkhourynathan/workouttracker_wrkpr"
         />
       </div>
       <div className="prose prose-neutral">
         <ProjectCard
-            title="Real Time Chat Application"
-            date="2024"
-            description="Developer a real time chat application which allows users to create a account and chat with each other."
-            techStack={['Python', 'PostgreSQL', 'Flask', 'Socket.io']}
-            imageUrl=""
-            githubUrl="https://github.com/elkhourynathan/chat_app"
+          title="Real Time Chat Application"
+          date="2024"
+          description="Developer a real time chat application which allows users to create a account and chat with each other."
+          techStack={['Python', 'PostgreSQL', 'Flask', 'Socket.io']}
+          imageUrl=""
+          githubUrl="https://github.com/elkhourynathan/chat_app"
         />
       </div>
       <div className="prose prose-neutral">
         <ProjectCard
-            title="Algorithmic Infared Image Detection"
-            date="2024"
-            description="Created a python application which takes in infared image frames and detects heat signatures."
-            techStack={['Python', 'Pygame']}
-            imageUrl="/images/marked_target.png"
-            githubUrl="https://github.com/elkhourynathan/python-detectheat"
+          title="Algorithmic Infared Image Detection"
+          date="2024"
+          description="Created a python application which takes in infared image frames and detects heat signatures."
+          techStack={['Python', 'Pygame']}
+          imageUrl="/images/marked_target.png"
+          githubUrl="https://github.com/elkhourynathan/python-detectheat"
         />
       </div>
     </section>
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -26,6 +26,12 @@ export default function WorkPage() {
           Working on the FALC-AI team, developing computer vision based
           solutions for retail, construction and other industries.
         </p>
+        <ul>
+          <li>
+            Integrated parallelism into the computer vision pipeline to fully
+            utilize edge device hardware, increasing FPS by 30%
+          </li>
+        </ul>
         <div className="space-x-2">
           <div className="badge badge-accent badge-outline">Python</div>
           <div className="badge badge-accent badge-outline">PyTorch</div>
@@ -37,29 +43,26 @@ export default function WorkPage() {
           Apption Software
         </h2>
         <div className="flex flex-row items-start justify-between">
-          <p className="text-neutral-400 text-sm">Full Stack Developer Intern</p>
+          <p className="text-neutral-400 text-sm">
+            Full Stack Developer Intern
+          </p>
           <p className="text-neutral-400 text-sm">Sept 2023 - Dec 2023</p>
         </div>
         <ul>
           <li>
-            Developed a keyword extraction algorithm in C# to extract critical
-            keywords and enable access to unstructured documents, enhancing
-            solution speed by 80% with more contextually relevant results
+            Developed a keyword extraction algorithm in C# using tokenization,
+            word co-occurrence, position analysis, and inverse frequency,
+            resulting in an 5x increase in computation speed for accessing
+            structured and unstructured documents
           </li>
           <li>
             Engineered the Datahunter Query Language (DQL), a user focused query
             language using a modified command pattern for backend architecture,
-            enhancing data retrieval efficiency and query capabilities
+            resulting in over 200% faster query loads and query capabilities
           </li>
           <li>
-            Rebuilt Datahunter’s discover page using Blazor to integrate DQL,
-            resulting in a more intuitive user interface, over 200% faster query
-            loads, and new features such as saved query histories
-          </li>
-          <li>
-            Overhauled the data catalogue pipeline in Databricks with data
-            engineering, automation, and establishing secure, scalable data
-            export with encryption and hashing to Azure storage
+            Overhauled the data catalog pipeline in Databricks, resulting in
+            secure and automated data exports to Azure storage
           </li>
         </ul>
         <div className="space-x-2">
@@ -79,28 +82,18 @@ export default function WorkPage() {
         </div>
         <ul>
           <li>
-            Designed and developed a comprehensive test automation program for
-            optical transceivers using Python, reducing testing time by over 10
-            hours per week
+          Lead the developed of a comprehensive test automation program for optical transceivers using Python, saving over 20 hours of testing time per week
           </li>
           <li>
-            Seamlessly integrated hardware components by implementing APIs from
-            BERTs, DSOs, and temperature control units
+          Integrated hardware components by utilizing C++ APIs in Python via wrappers for BERTs, DSOs, and temperature control units, ensuring seamless functionality
           </li>
           <li>
-            Developed testing logic with extensive error checking mechanisms to
-            guarantee correct hardware connections and disconnections,
-            mitigating potential test disruptions
-          </li>
-          <li>
-            Spearheaded the design of a user-centric interface with ReactJS and
-            Electron, displaying real-time data from the hardware and
-            streamlining all relevant data into a structured downloadable report
-            for end-users
+          Designed a user-centric interface with ReactJS and Electron, displaying real-time hardware data and streamlining it into downloadable reports
           </li>
         </ul>
         <div className="space-x-2">
           <div className="badge badge-accent badge-outline">Python</div>
+          <div className="badge badge-accent badge-outline">C++</div>
           <div className="badge badge-accent badge-outline">Reactjs</div>
           <div className="badge badge-accent badge-outline">Electron</div>
           <div className="badge badge-accent badge-outline">Flask</div>
